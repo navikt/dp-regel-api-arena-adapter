6ffdc5b134c94ffe4968dce75a108035bb4a8243
Geir André Lund <geir.andre.lund@nav.no>
2019-01-22 16:16:29 +0100

    Refactored tests around scenarios - Combined all scenario tests to same test class - Created "minsteinntekt" assertion - match request class

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/RegelApiDummy.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/RegelApiDummy.kt
@@ -11,33 +11,34 @@ class RegelApiDummy : RegelApiClient {
 
     override fun startMinsteinntktBeregning(request: MinsteinntektBeregningsRequest): URI {
         return when {
-            matchesScenario1_1(request) -> {
+            Scenario1_1Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario1_1")
             }
-            matchesScenario1_2(request) -> {
+            Scenario1_2Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario1_2")
             }
-            matchesScenario1_3(request) -> {
+            Scenario1_3Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario1_3")
             }
-            matchesScenario2_1(request) || matchesScenario2_3(request) -> {
+            Scenario2_1_AND_3_Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario2_1-3")
             }
-            matchesScenario2_2(request) -> {
+            Scenario2_2Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario2_2")
             }
-            matchesScenario3_1(request) -> {
+            Scenario3_1Request == request -> {
                 currentRequest = request
                 URI.create("URN:scenario3_1")
             }
             else -> {
                 currentRequest = request
-                URI.create("URN:nomatch") }
+                URI.create("URN:nomatch")
+            }
         }
     }
 
@@ -139,7 +140,8 @@ class RegelApiDummy : RegelApiClient {
                 InntektsPeriode("2018-02", "2019-01"),
                 InntektsPeriode("2017-02", "2018-01"),
                 InntektsPeriode("2016-02", "2017-01"),
-                Inntekt(BigDecimal(100000), BigDecimal(40000), BigDecimal(0), inneholderNaeringsinntekter = false))
+                Inntekt(BigDecimal(100000), BigDecimal(40000), BigDecimal(0), inneholderNaeringsinntekter = false)
+            )
         }
     }
 
@@ -156,27 +158,50 @@ class RegelApiDummy : RegelApiClient {
     }
 }
 
-fun matchesScenario1_1(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000033752789" && request.vedtakId == 31018297 && request.beregningsdato == "2019-01-10"
-
-fun matchesScenario1_2(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000033752789" && request.vedtakId == 31018297 && request.beregningsdato == "2019-02-06" && !request.oppfyllerKravTilFangstOgFisk
-
-fun matchesScenario1_3(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000033752789" && request.vedtakId == 31018297 && request.beregningsdato == "2019-02-06" && request.oppfyllerKravTilFangstOgFisk
-
-fun matchesScenario2_1(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000003221752" && request.vedtakId == 31018347 && request.beregningsdato == "2019-01-11"
-fun matchesScenario2_2(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000003221752" && request.vedtakId == 31018347 && request.beregningsdato == "2019-02-07"
-fun matchesScenario2_3(request: MinsteinntektBeregningsRequest) = request.aktorId == "1000003221752" && request.vedtakId == 31018347 && request.beregningsdato == "2019-01-11"
-fun matchesScenario3_1(request: MinsteinntektBeregningsRequest) = request == Scenario3_1Request
-
 fun mapRequestToParametere(request: MinsteinntektBeregningsRequest, inntektsId: String): Parametere =
     Parametere(
         request.aktorId,
         request.vedtakId,
         request.beregningsdato,
         inntektsId,
-        request.bruktinntektsPeriode?.let { InntektsPeriode(request.bruktinntektsPeriode.foersteMaaned, request.bruktinntektsPeriode.sisteMaaned) },
+        request.bruktinntektsPeriode?.let {
+            InntektsPeriode(
+                request.bruktinntektsPeriode.foersteMaaned,
+                request.bruktinntektsPeriode.sisteMaaned
+            )
+        },
         request.harAvtjentVerneplikt,
         request.oppfyllerKravTilFangstOgFisk
     )
+
+val Scenario1_1Request = MinsteinntektBeregningsRequest(
+    aktorId = "1000033752789",
+    vedtakId = 31018297,
+    beregningsdato = "2019-01-10",
+    harAvtjentVerneplikt = false,
+    oppfyllerKravTilFangstOgFisk = false
+)
+
+val Scenario1_2Request = Scenario1_1Request.copy(beregningsdato = "2019-02-06")
+val Scenario1_3Request = Scenario1_2Request.copy(
+    oppfyllerKravTilFangstOgFisk = true
+)
+
+val Scenario2_1_AND_3_Request = MinsteinntektBeregningsRequest(
+    aktorId = "1000003221752",
+    vedtakId = 31018347,
+    beregningsdato = "2019-01-11",
+    harAvtjentVerneplikt = false,
+    oppfyllerKravTilFangstOgFisk = false
+)
+
+val Scenario2_2Request = MinsteinntektBeregningsRequest(
+    aktorId = "1000003221752",
+    vedtakId = 31018347,
+    beregningsdato = "2019-02-07",
+    harAvtjentVerneplikt = false,
+    oppfyllerKravTilFangstOgFisk = false
+)
 
 val Scenario3_1Request = MinsteinntektBeregningsRequest(
     aktorId = "1000074474453",
---
+++ src/test/kotlin/no/nav/dagpenger/regel/api/arena/adapter/ScenariosTest.kt
@@ -0,0 +1,124 @@
+package no.nav.dagpenger.regel.api.arena.adapter
+
+import com.google.gson.Gson
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationCall
+import io.ktor.server.testing.TestApplicationEngine
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
+import io.ktor.server.testing.withTestApplication
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
+
+class ScenariosTest {
+
+    // Test and dummy responses as defined in https://confluence.adeo.no/display/TEAMARENA/ARENA-1892+-+04+Scenarier
+
+    @Test
+    fun `scenario 1-1`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario1_1Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = false, periode = 0, inntektsId = "A", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 1-2`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario1_2Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = true, periode = 52, inntektsId = "B", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 1-3`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario1_3Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = true, periode = 104, inntektsId = "B", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 2-1`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario2_1_AND_3_Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = true, periode = 52, inntektsId = "C", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 2-2`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario2_2Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = false, periode = 0, inntektsId = "D", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 2-3`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario2_1_AND_3_Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = true, periode = 52, inntektsId = "C", beregning = response)
+        }
+    }
+
+    @Test
+    fun `scenario 3_1`() = testApp {
+        handleRequest(HttpMethod.Post, "/minsteinntekt") {
+            addHeader(HttpHeaders.ContentType, "application/json")
+            setBody(Gson().toJson(Scenario3_1Request))
+        }.apply {
+            assert200OkResponse()
+            val response = Gson().fromJson(response.content, MinsteinntektBeregningsResponse::class.java)
+            assertMinsteinntektResponse(oppfyllerKrav = false, periode = 0, inntektsId = "J", beregning = response)
+        }
+    }
+
+    fun testApp(callback: TestApplicationEngine.() -> Unit) {
+        withTestApplication({ regelApiAdapter(RegelApiDummy()) }) { callback() }
+    }
+
+    fun assertMinsteinntektResponse(
+        oppfyllerKrav: Boolean,
+        periode: Int,
+        inntektsId: String,
+        beregning: MinsteinntektBeregningsResponse
+    ) {
+        Assertions.assertEquals(oppfyllerKrav, beregning.utfall.oppfyllerKravTilMinsteArbeidsinntekt)
+        Assertions.assertEquals(periode, beregning.utfall.periodeAntallUker)
+        Assertions.assertEquals(inntektsId, beregning.parametere.inntektsId)
+        assertNotNull("Beregnings id er satt og unik", beregning.beregningsId)
+    }
+
+    fun TestApplicationCall.assert200OkResponse() {
+        Assertions.assertTrue(requestHandled)
+        Assertions.assertEquals(HttpStatusCode.OK, response.status())
+    }
+}
