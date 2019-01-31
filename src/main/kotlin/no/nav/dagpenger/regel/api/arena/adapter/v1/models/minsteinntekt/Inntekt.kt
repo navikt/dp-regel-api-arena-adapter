dcc4d6aafaf19be3a9b5816aab82be4583465be3
Geir André Lund <geir.andre.lund@nav.no>
2019-01-31 09:45:48 +0100

    Fix syntax error

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/minsteinntekt/Inntekt.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/minsteinntekt/Inntekt.kt
@@ -5,10 +5,11 @@ data class Inntekt(
     val periode: Int, // todo: enum?
     val inneholderNaeringsinntekter: Boolean,
     val andel: Int
-)
-init {
- val gyldigePerioder = setOf(1,2,3)
+) {
+    init {
+        val gyldigePerioder = setOf(1, 2, 3)
         if (!gyldigePerioder.contains(periode)) {
             throw IllegalArgumentException("Ugyldig periode for inntektgrunnlat, gyldige verdier er ${gyldigePerioder.joinToString { "$it" }}")
         }
+    }
 }
