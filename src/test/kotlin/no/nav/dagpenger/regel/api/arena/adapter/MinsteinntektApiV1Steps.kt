952fc2a390a0606c013eadd16e5de8039294c664
Tonje Mjøvik <tonje.mjovik@nav.no>
2019-01-31 10:34:45 +0100

    add scenarioes

--- src/test/kotlin/no/nav/dagpenger/regel/api/arena/adapter/MinsteinntektApiV1Steps.kt
+++ src/test/kotlin/no/nav/dagpenger/regel/api/arena/adapter/MinsteinntektApiV1Steps.kt
@@ -55,6 +55,126 @@ class MinsteinntektApiV1Steps : No {
                 assertEquals(periodeAntallUker, minsteinntektBeregning.resultat.periodeAntallUker)
             }
         }
+
+        Gitt("at søker har ingen inntekt siste {int} måneder") { int1: Int ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Når("digidag skal vurdere minsteinntektkrav og periode") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("har avtjent verneplikt") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Når("digidag skal vrudere søknaden") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("er kravet til minsteinntekt {string}") { string: String ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("perioden er {int} uker") { int1: Int ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("grunnlaget er XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("satsen er XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("at grunnlag er satt manuelt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Når("digidag skal sette sats") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("er ukessats satt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("dagsats satt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("at søker har fått minsteinntekt oppfylt") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("søker skal få barnetilleg") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("er det lagt til {int} * {int} * antall barn på ukessats") { int1: Int, int2: Int ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("at søker har tjent {double}G siste {int} måneder") { double1: Double, int1: Int ->
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Når("digidag skal vurdere søknaden") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("grunnlaget er satt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("dagsatsen er satt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("ukessatsen er satt til XXX") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("at søker skal ha medberegnet inntekt fra fangst og fisk") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("tas denne inntekten med") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Gitt("at noe av inntekten er benyttet fra før") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
+
+        Så("skal denne trekkes fra") {
+            // Write code here that turns the phrase above into concrete actions
+            throw cucumber.api.PendingException()
+        }
     }
 
     fun <T> String?.parseJsonFrom(adapter: JsonAdapter<T>): T {
