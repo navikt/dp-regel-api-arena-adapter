85a47dbaae217afbad904604a544dcbc96205c49
Geir A. Lund <tilgeir@gmail.com>
2019-01-31 08:56:08 +0100

    Validate "perioder"

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/minsteinntekt/Inntekt.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/minsteinntekt/Inntekt.kt
@@ -6,3 +6,9 @@ data class Inntekt(
     val inneholderNaeringsinntekter: Boolean,
     val andel: Int
 )
+init {
+ val gyldigePerioder = setOf(1,2,3)
+        if (!gyldigePerioder.contains(periode)) {
+            throw IllegalArgumentException("Ugyldig periode for inntektgrunnlat, gyldige verdier er ${gyldigePerioder.joinToString { "$it" }}")
+        }
+}
