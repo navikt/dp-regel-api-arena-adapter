1ffc2bed556ec20182a1e63d0ba9c461057ea754
Atle Nordland <atle.nordland@nav.no>
2019-03-07 10:18:02 +0100

    Fix inntektsperiode json field name

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/common/InntektsPeriode.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/common/InntektsPeriode.kt
@@ -1,8 +1,9 @@
 package no.nav.dagpenger.regel.api.arena.adapter.v1.models.common
 
+import com.squareup.moshi.Json
 import java.time.YearMonth
 
 data class InntektsPeriode(
-    val foersteMaaned: YearMonth? = null,
-    val sisteMaaned: YearMonth
+    @Json(name = "førsteMåned") val foersteMaaned: YearMonth? = null,
+    @Json(name = "sisteMåned") val sisteMaaned: YearMonth
 )
