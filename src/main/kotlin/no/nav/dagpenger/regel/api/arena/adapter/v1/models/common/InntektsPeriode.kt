cef4468feb9fb71c4f09714cff3d8e338e84db63
Geir André Lund <geir.andre.lund@nav.no>
2019-03-08 13:47:10 +0100

    Revert "Fix inntektsperiode json field name"
    This reverts commit 1ffc2bed556ec20182a1e63d0ba9c461057ea754.

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/common/InntektsPeriode.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/models/common/InntektsPeriode.kt
@@ -1,9 +1,8 @@
 package no.nav.dagpenger.regel.api.arena.adapter.v1.models.common
 
-import com.squareup.moshi.Json
 import java.time.YearMonth
 
 data class InntektsPeriode(
-    @Json(name = "førsteMåned") val foersteMaaned: YearMonth? = null,
-    @Json(name = "sisteMåned") val sisteMaaned: YearMonth
+    val foersteMaaned: YearMonth? = null,
+    val sisteMaaned: YearMonth
 )
