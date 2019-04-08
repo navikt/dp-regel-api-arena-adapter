342351e3f59b8c034d0f74f9c62813844b8c0ab1
Geir André Lund <geir.andre.lund@nav.no>
2019-04-08 14:35:58 +0200

    Fix uat tests

--- src/uatTests/kotlin/no/nav/dagpenger/regel/api/arena/adapter/DagpengergrunnlagApiV1Steps.kt
+++ src/uatTests/kotlin/no/nav/dagpenger/regel/api/arena/adapter/DagpengergrunnlagApiV1Steps.kt
@@ -46,8 +46,6 @@ class DagpengergrunnlagApiV1Steps : No {
 
         Når("digidag skal beregne grunnlag") {
             withTestApplication({ regelApiAdapter(
-                mockk(),
-                mockk(),
                 synchronousMinsteinntekt,
                 synchronousPeriode,
                 synchronousGrunnlag,
