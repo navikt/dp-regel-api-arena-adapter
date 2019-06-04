f0753209de39a1c90f44f09fa3429ba2d9ae401a
Atle Nordland <atle.nordland@nav.no>
2019-06-04 16:05:36 +0200

    revert revert

--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/Exceptions.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/Exceptions.kt
@@ -1,4 +1,5 @@
 package no.nav.dagpenger.regel.api.arena.adapter.v1
 
-class UnMatchingFaktumException(override val message: String) : RuntimeException(message)
+class FeilBeregningsregelException(message: String) : RuntimeException(message)
+class MissingSubsumsjonDataException(message: String) : RuntimeException(message)
 class InvalidInnteksperiodeException(override val message: String) : RuntimeException(message)
--- src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/GrunnlagOgSatsApi.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/arena/adapter/v1/GrunnlagOgSatsApi.kt
@@ -7,134 +7,37 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.post
 import io.ktor.routing.route
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.GrunnlagBeregningsregel
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.Grunnlag
 import no.nav.dagpenger.regel.api.arena.adapter.v1.models.GrunnlagOgSatsParametere
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.GrunnlagOgSatsRegelFaktum
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.GrunnlagOgSatsResultat
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.GrunnlagOgSatsSubsumsjon
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.Inntekt
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.InntektsPeriode
-import no.nav.dagpenger.regel.api.arena.adapter.v1.models.Sats
-import no.nav.dagpenger.regel.api.internal.grunnlag.SynchronousGrunnlag
-import no.nav.dagpenger.regel.api.internal.models.GrunnlagFaktum
-import no.nav.dagpenger.regel.api.internal.models.GrunnlagResultat
-import no.nav.dagpenger.regel.api.internal.models.GrunnlagSubsumsjon
-import no.nav.dagpenger.regel.api.internal.models.SatsFaktum
-import no.nav.dagpenger.regel.api.internal.models.SatsSubsumsjon
-import no.nav.dagpenger.regel.api.internal.sats.SynchronousSats
+import no.nav.dagpenger.regel.api.internal.BehovRequest
+import no.nav.dagpenger.regel.api.internal.SynchronousSubsumsjonClient
+import no.nav.dagpenger.regel.api.internal.extractGrunnlagOgSats
 
 fun Route.GrunnlagOgSatsApi(
-    synchronousGrunnlag: SynchronousGrunnlag,
-    synchronousSats: SynchronousSats
+    synchronousSubsumsjonClient: SynchronousSubsumsjonClient
 ) {
 
     route("/dagpengegrunnlag") {
         post {
             val parametere = call.receive<GrunnlagOgSatsParametere>()
 
-            val grunnlagSubsumsjon = synchronousGrunnlag.getGrunnlagSynchronously(parametere)
-
-            val satsSubsumsjon = synchronousSats.getSatsSynchronously(parametere)
+            val behovRequest = behovFromParametere(parametere)
 
             val grunnlagOgSatsSubsumsjon =
-                mergeGrunnlagOgSatsSubsumsjon(
-                    grunnlagSubsumsjon,
-                    satsSubsumsjon
-                )
+                synchronousSubsumsjonClient.getSubsumsjonSynchronously(behovRequest, ::extractGrunnlagOgSats)
 
             call.respond(HttpStatusCode.OK, grunnlagOgSatsSubsumsjon)
         }
     }
 }
 
-fun mergeGrunnlagOgSatsSubsumsjon(
-    grunnlagSubsumsjon: GrunnlagSubsumsjon,
-    satsSubsumsjon: SatsSubsumsjon
-): GrunnlagOgSatsSubsumsjon {
-
-    val grunnlagFaktum = grunnlagSubsumsjon.faktum
-    val grunnlagResultat = grunnlagSubsumsjon.resultat
-
-    val satsFaktum = satsSubsumsjon.faktum
-    val satsResultat = satsSubsumsjon.resultat
-
-    if (!compareFields(
-            grunnlagFaktum,
-            satsFaktum
-        )
-    ) throw UnMatchingFaktumException("Grunnlag and sats faktum dont match")
-
-    return GrunnlagOgSatsSubsumsjon(
-        grunnlagSubsumsjon.subsumsjonsId,
-        satsSubsumsjon.subsumsjonsId,
-        grunnlagSubsumsjon.opprettet,
-        grunnlagSubsumsjon.utfort,
-        GrunnlagOgSatsRegelFaktum(
-            grunnlagFaktum.aktorId,
-            grunnlagFaktum.vedtakId,
-            grunnlagFaktum.beregningsdato,
-            grunnlagFaktum.inntektsId,
-            grunnlagFaktum.harAvtjentVerneplikt,
-            grunnlagFaktum.oppfyllerKravTilFangstOgFisk,
-            satsFaktum.antallBarn,
-            grunnlagFaktum.manueltGrunnlag
-        ),
-        GrunnlagOgSatsResultat(
-            Grunnlag(
-                grunnlagResultat.avkortet,
-                grunnlagResultat.uavkortet
-            ),
-            Sats(satsResultat.dagsats, satsResultat.ukesats),
-            findBeregningsregel(grunnlagResultat),
-            satsResultat.benyttet90ProsentRegel
-        ),
-        grunnlagSubsumsjon.inntekt?.map {
-            Inntekt(
-                it.inntekt,
-                it.periode,
-                InntektsPeriode(
-                    it.inntektsPeriode.førsteMåned,
-                    it.inntektsPeriode.sisteMåned
-                ),
-                it.inneholderFangstOgFisk
-            )
-        }?.toSet(),
-        grunnlagFaktum.inntektManueltRedigert,
-        grunnlagFaktum.inntektAvvik
-
+fun behovFromParametere(parametere: GrunnlagOgSatsParametere): BehovRequest {
+    return BehovRequest(
+            parametere.aktorId,
+            parametere.vedtakId,
+            parametere.beregningsdato,
+            harAvtjentVerneplikt = parametere.harAvtjentVerneplikt,
+            oppfyllerKravTilFangstOgFisk = parametere.oppfyllerKravTilFangstOgFisk,
+            manueltGrunnlag = parametere.grunnlag,
+            antallBarn = parametere.antallBarn
     )
 }
-
-fun findBeregningsregel(grunnlagResultat: GrunnlagResultat): GrunnlagBeregningsregel {
-
-    return when {
-        grunnlagResultat.beregningsregel == "Manuell" && grunnlagResultat.harAvkortet -> GrunnlagBeregningsregel.MANUELL_OVER_6G
-        grunnlagResultat.beregningsregel == "Manuell" -> GrunnlagBeregningsregel.MANUELL_UNDER_6G
-        grunnlagResultat.beregningsregel == "ArbeidsinntektSiste12" && grunnlagResultat.harAvkortet -> GrunnlagBeregningsregel.ORDINAER_OVER_6G_SISTE_2019
-        grunnlagResultat.beregningsregel == "ArbeidsinntektSiste12" -> GrunnlagBeregningsregel.ORDINAER_ETTAAR
-        grunnlagResultat.beregningsregel == "FangstOgFiskSiste12" && grunnlagResultat.harAvkortet -> GrunnlagBeregningsregel.ORDINAER_OVER_6G_SISTE_2019
-        grunnlagResultat.beregningsregel == "FangstOgFiskSiste12" -> GrunnlagBeregningsregel.ORDINAER_ETTAAR
-        grunnlagResultat.beregningsregel == "ArbeidsinntektSiste36" && grunnlagResultat.harAvkortet -> GrunnlagBeregningsregel.ORDINAER_OVER_6G_3SISTE_2019
-        grunnlagResultat.beregningsregel == "ArbeidsinntektSiste36" -> GrunnlagBeregningsregel.ORDINAER_TREAAR
-        grunnlagResultat.beregningsregel == "FangstOgFiskSiste36" && grunnlagResultat.harAvkortet -> GrunnlagBeregningsregel.ORDINAER_OVER_6G_3SISTE_2019
-        grunnlagResultat.beregningsregel == "FangstOgFiskSiste36" -> GrunnlagBeregningsregel.ORDINAER_TREAAR
-        grunnlagResultat.beregningsregel == "Verneplikt" -> GrunnlagBeregningsregel.VERNEPLIKT
-        grunnlagResultat.beregningsregel == "Manuell under 6G" -> GrunnlagBeregningsregel.MANUELL_UNDER_6G
-        grunnlagResultat.beregningsregel == "Manuell over 6G" -> GrunnlagBeregningsregel.MANUELL_OVER_6G
-        else -> throw FeilBeregningsregelException("Ukjent beregningsregel: '${grunnlagResultat.beregningsregel}'")
-    }
-}
-
-fun compareFields(grunnlagFaktum: GrunnlagFaktum, satsFaktum: SatsFaktum): Boolean {
-
-    if (grunnlagFaktum.aktorId.equals(satsFaktum.aktorId) &&
-        grunnlagFaktum.vedtakId.equals(satsFaktum.vedtakId) &&
-        grunnlagFaktum.beregningsdato.equals(satsFaktum.beregningsdato)
-    ) {
-        return true
-    }
-    return false
-}
-
-class FeilBeregningsregelException(message: String) : RuntimeException(message)
