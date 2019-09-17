a032df11ac67caaf9be27d2f5e47f8625c19409b
Giao The Cung <giaothe@gmail.com>
2019-09-17 16:38:28 +0200

    Remove uat tests from project.

--- src/uatTests/kotlin/no/nav/dagpenger/regel/api/arena/adapter/Utils.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.dagpenger.regel.api.arena.adapter
-
-import com.squareup.moshi.JsonAdapter
-
-fun <T> String?.parseJsonFrom(adapter: JsonAdapter<T>): T {
-    return this?.let { adapter.fromJson(it) } ?: throw AssertionError("No content from server")
-}
