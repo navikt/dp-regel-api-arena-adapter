572f97b418f6c0f8f7335d02fb6175baef490331
Geir André Lund <geir.andre.lund@nav.no>
2020-08-18 08:07:06 +0200

    Bytt til dp-inntekt i teamdagpenger namespace navikt/dagpenger#531

--- src/test/kotlin/no/nav/dagpenger/regel/api/ConfigurationTest.kt
+++ src/test/kotlin/no/nav/dagpenger/regel/api/ConfigurationTest.kt
@@ -32,7 +32,7 @@ internal class ConfigurationTest {
                 this.application.profile shouldBe Profile.PROD
                 this.application.jwksIssuer shouldBe "https://security-token-service.nais.adeo.no"
                 this.application.jwksUrl shouldBe "http://security-token-service/rest/v1/sts/jwks"
-                this.application.dpInntektApiUrl shouldBe "http://dp-inntekt-api"
+                this.application.dpInntektApiUrl shouldBe "http://dp-inntekt-api.teamdagpenger"
                 this.application.dpRegelApiUrl shouldBe "http://dp-regel-api"
             }
         }
