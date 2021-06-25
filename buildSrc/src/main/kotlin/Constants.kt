e3da34881fb4939e6cf51a0f9a28dc60d693aa72
Geir André Lund <geir.andre.lund@nav.no>
2021-06-25 10:29:26 +0200

    Oppdatere 3. parts biblioteker

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -133,7 +133,7 @@ object Kotlin {
 }
 
 object KoTest {
-    const val version = "4.4.1"
+    const val version = "4.6.0"
 
     // for kotest framework
     const val runner = "io.kotest:kotest-runner-junit5-jvm:$version"
@@ -229,7 +229,7 @@ object Ktlint {
 }
 
 object Spotless {
-    const val version = "5.10.1"
+    const val version = "5.14.0"
     const val spotless = "com.diffplug.spotless"
 }
 
@@ -259,7 +259,7 @@ object Wiremock {
 }
 
 object Graphql {
-    const val version = "4.0.0-alpha.12"
+    const val version = "4.1.1"
     const val graphql = "com.expediagroup.graphql"
     val client = library("client")
     fun library(name: String) = "com.expediagroup:graphql-kotlin-$name:$version"
