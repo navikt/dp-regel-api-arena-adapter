584fa16f6c3b35a194b3f983b65665a01e50d4e7
Geir André Lund <geir.andre.lund@nav.no>
2023-04-27 11:01:24 +0200

    Fjernet ubrukt kode

--- src/main/kotlin/no/nav/dagpenger/regel/api/serder/ObjectMapper.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/api/serder/ObjectMapper.kt
@@ -8,23 +8,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 internal val jacksonObjectMapper = jacksonObjectMapper().also {
     it.registerModule(JavaTimeModule())
-//    it.registerModule(
-//        SimpleModule().also { module ->
-//            module.addSerializer(Ulid::class.java, UlidSerializer())
-//            module.addDeserializer(Ulid::class.java, UlidDeserializer())
-//        }
-//    )
     it.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     it.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
     it.setSerializationInclusion(JsonInclude.Include.NON_NULL)
 }
-
-//
-// private class UlidSerializer : JsonSerializer<Ulid>() {
-//    override fun serialize(value: Ulid, gen: JsonGenerator, serializers: SerializerProvider) =
-//        gen.writeString(value.id)
-// }
-//
-// private class UlidDeserializer : JsonDeserializer<Ulid>() {
-//    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): Ulid = Ulid(p.text)
-// }
